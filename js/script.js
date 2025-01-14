ca5dc745a41c267e020ff9d7e5c66c99fe379b68
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-14 13:54:12 +0100

    general layout

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// BONUS:
+// Inseriamo la validazione: se l’utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+// Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
